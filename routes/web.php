80ba0192da483e26eb4be7827d53466c9a454b01
walissonhms <walissonhsp@outlook.com>
2020-02-26 22:37:49 -0300

    [Command Artisan Route] Listando Rotas com Artisan

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,23 @@ Route::get('/idade', 'MeuControlador@getIdade');
 Route::get('/multiplicar/{n1}/{n2}', 'MeuControlador@multiplicar');
 
 //Associando Rotas ao Controlador
+/* Command: php artisan route:list
++--------+-----------+-------------------------+------------------+-------------------------------------------------+--------------+
+| Domain | Method    | URI                     | Name             | Action                                          | Middleware   |
++--------+-----------+-------------------------+------------------+-------------------------------------------------+--------------+
+|        | GET|HEAD  | /                       |                  | Closure                                         | web          |
+|        | GET|HEAD  | api/user                |                  | Closure                                         | api,auth:api |
+|        | GET|HEAD  | clientes                | clientes.index   | App\Http\Controllers\ClienteControlador@index   | web          |
+|        | POST      | clientes                | clientes.store   | App\Http\Controllers\ClienteControlador@store   | web          |
+|        | GET|HEAD  | clientes/create         | clientes.create  | App\Http\Controllers\ClienteControlador@create  | web          |
+|        | GET|HEAD  | clientes/{cliente}      | clientes.show    | App\Http\Controllers\ClienteControlador@show    | web          |
+|        | PUT|PATCH | clientes/{cliente}      | clientes.update  | App\Http\Controllers\ClienteControlador@update  | web          |
+|        | DELETE    | clientes/{cliente}      | clientes.destroy | App\Http\Controllers\ClienteControlador@destroy | web          |
+|        | GET|HEAD  | clientes/{cliente}/edit | clientes.edit    | App\Http\Controllers\ClienteControlador@edit    | web          |
+|        | GET|HEAD  | idade                   |                  | App\Http\Controllers\MeuControlador@getIdade    | web          |
+|        | GET|HEAD  | multiplicar/{n1}/{n2}   |                  | App\Http\Controllers\MeuControlador@multiplicar | web          |
+|        | GET|HEAD  | nome                    |                  | App\Http\Controllers\MeuControlador@getNome     | web          |
+|        | GET|HEAD  | produtos                |                  | App\Http\Controllers\MeuControlador@produtos    | web          |
++--------+-----------+-------------------------+------------------+-------------------------------------------------+--------------+
+*/
 Route::resource('/clientes', 'ClienteControlador');
